5259ba5d6b3e4ae8844dda13e69cab6bee0c4dc2
Aaron Kawer <aaronkawer@gmail.com>
2015-03-22 10:25:19 -0600

    added Navbar and edited syntax

--- MiCampusApp/estacionamiento.php
+++ MiCampusApp/estacionamiento.php
@@ -12,11 +12,41 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 <title>Mi Campus Disponible</title>
-<h1> Estacionamiento </h1>
+
 
 </head>
 
 <body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">Mi Campus Disponible</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="#">Contacto </a></li>
+        <li><a href="#">Login</a></li>
+        
+      </ul>
+    
+      
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<h1> Estacionamiento </h1>
 
 <a class="btn btn-primary" href="http://micampusdisponible.herobo.com/estudio.php" role="button">Estudio</a>
 <a class="btn btn-success" href="http://micampusdisponible.herobo.com/alimentacion.php" role="button">Alimentación</a>
@@ -36,7 +66,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "select l.nombre as nombre, count(l.id) as cantidad, l.capacidad as capacidad from individuales i, lugar l where i.lugar = l.id and l.area = 3 and i.estado = 1 group by l.id";
+$sql = "select l.nombre as nombre, g.ocupacion as cantidad, l.capacidad as capacidad from grupales g, lugar l where g.lugar = l.id and l.area = 3";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
--- MiCampusApp/residencias.php
+++ MiCampusApp/residencias.php
@@ -12,11 +12,41 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
 <title>Mi Campus Disponible</title>
-<h1> Residencias </h1>
+
 
 </head>
 
 <body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.html">Mi Campus Disponible</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li ><a href="#">Contacto </a></li>
+        <li><a href="#">Login</a></li>
+        
+      </ul>
+    
+      
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<h1> Residencias </h1>
 
 <a class="btn btn-primary" href="http://micampusdisponible.herobo.com/estudio.php" role="button">Estudio</a>
 <a class="btn btn-success" href="http://micampusdisponible.herobo.com/alimentacion.php" role="button">Alimentación</a>
